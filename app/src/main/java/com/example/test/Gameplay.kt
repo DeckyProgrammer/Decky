fa04663659391f0075da9ceb850e962e22a1a410
SigismondVladimir <gvidelaine@hotmail.com>
2023-06-07 10:07:30 +0200

    Réparation des conneries de Lucas

--- app/src/main/java/com/example/test/Gameplay.kt
+++ app/src/main/java/com/example/test/Gameplay.kt
@@ -1,4 +1,11 @@
 package com.example.test
 
-class Gameplay {
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Gameplay : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gameplay)
+    }
 }
---
+++ app/src/main/java/com/example/test/noms2.kt
@@ -0,0 +1,19 @@
+package com.example.test
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class noms2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_noms2)
+
+        val button = findViewById<Button>(R.id.button_valider)
+        button.setOnClickListener {
+            val intent = Intent(this, Gameplay::class.java)
+            startActivity(intent)
+        }
+    }
+}

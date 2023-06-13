4f163e09227ef6c0a2b7a2bc420ae76383a602be
DeckyCreator <arkadinleague99@gmail.com>
2023-06-13 11:47:09 +0200

    Update 13/06 n2

--- app/src/main/java/com/example/test/Gameplay.kt
+++ app/src/main/java/com/example/test/Gameplay.kt
@@ -1,11 +1,19 @@
 package com.example.test
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Gameplay : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gameplay)
+
+        val buttonpass = findViewById<Button>(R.id.button_pass)
+        buttonpass.setOnClickListener {
+            val intent = Intent(this, annoncetour::class.java)
+            startActivity(intent)
+        }
     }
 }
